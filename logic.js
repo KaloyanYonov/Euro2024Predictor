806b92d805018be44b593b38e2d3d4ced193095b
Kaloyan Yonov <kaloqntyu@gmail.com>
2024-01-18 21:40:02 +0200

    Updated logic.js

--- logic.js
+++ logic.js
@@ -11,34 +11,5 @@ function selectTeam(button) {
     const groupName = group.querySelector('h2').textContent;
     const teamName = button.textContent;
 
-    // Check if the team is already selected
-    if (selectedTeams[groupName].includes(teamName)) {
-        // If the team is already selected, remove it from the list
-        selectedTeams[groupName] = selectedTeams[groupName].filter(t => t !== teamName);
-        // Remove the box of the first team selected if there are more than 2 teams selected
-        if (selectedTeams[groupName].length > 2) {
-            const firstTeamToRemove = selectedTeams[groupName][0];
-            removeTeamBox(groupName, firstTeamToRemove);
-        }
-    } else {
-        // If the team is not selected, add it to the list
-        selectedTeams[groupName].push(teamName);
-        // If more than 2 teams are selected, remove the box of the first team selected
-        if (selectedTeams[groupName].length > 2) {
-            const firstTeamToRemove = selectedTeams[groupName][0];
-            removeTeamBox(groupName, firstTeamToRemove);
-        }
-    }
-
-    // Update the display or perform additional actions if needed
-    console.log(`Selected ${teamName} in ${groupName}`);
-}
-
-function removeTeamBox(groupName, teamName) {
-    // Find and remove the box of the specified team
-    const group = document.querySelector(`.Group h2:contains('${groupName}')`).parentNode;
-    const teamBoxToRemove = group.querySelector(`li:contains('${teamName}')`);
-    if (teamBoxToRemove) {
-        teamBoxToRemove.remove();
-    }
+    // nothing yet
 }
